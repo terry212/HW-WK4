c52ac441015ac3b65c290f537be3d502eb0a8fc8
Terry C <terrycetoute@gmail.com>
2020-04-25 23:10:11 -0400

    refactored code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Set the body to a variable
 var body = document.body;
-
-// Declare variables that will be grabbing query selectors
 // Top bar variables
 var topBar = document.querySelector("#top-status-bar");
 var highscoreDiv = document.querySelector("#view-highscore");
@@ -10,18 +8,15 @@ var timerDiv = document.querySelector("#time-remain");
 var titleDiv = document.querySelector("#bold-header");
 // Content div
 var contentDiv = document.querySelector("#content-area");
-// var bodyMain = document.querySelector("main");
-
+// create variable for the display section
+var resultDiv = document.querySelector("#result-display");
 // Timer variable
 var secondsLeft = 60;
-
-function deduct() {
-    secondsLeft -= 10;
-    // return secondsLeft;
-}
-
+// timer deduction function
+function deduct() { secondsLeft -= 10; };
 // Score variable
 var score = 0;
+
 // Array of questions
 var questions = [
     { q: "What is missing in the following tag: <h1></> ?", options: ["h1", "h2", "Nothing", ";"], correctAns: "ans1" },
@@ -45,6 +40,8 @@ var optionA = document.createElement("button");
 var optionB = document.createElement("button");
 var optionC = document.createElement("button");
 var optionD = document.createElement("button");
+// Display of whether choice is right or wrong elements are created below
+var resultPTag = document.createElement("p");
 
 // Adding relative content
 tbATag.textContent = "View Highscores";
@@ -61,22 +58,27 @@ topBar.appendChild(tbHrTag);
 titleDiv.appendChild(titleTag);
 contentDiv.appendChild(contentPTag);
 contentDiv.appendChild(contentStartBtn);
+resultDiv.appendChild(resultPTag);
 
 // Style elements
 tbATag.setAttribute("href", "#");
-tbATag.setAttribute("style", "margin-left: 5%;margin-top: 3%;font-size:14pt;float: left; height:auto;");
-tbPTag.setAttribute("style", "margin-right: 5%;margin-top: 3%;font-size:14pt;float: right; height:auto;");
+tbATag.setAttribute("style", "margin-left: 5%;margin-top: 3%; font-size:14pt; float: left; height:auto;");
+tbPTag.setAttribute("style", "margin-right: 5%;margin-top: 3%; font-size:14pt; float: right; height:auto;");
 tbHrTag.setAttribute("style", "margin-top: 1rem; margin-bottom: 1rem; border: 0; border-bottom: 1px solid rgba(0,0,0,.1);");
 titleTag.setAttribute("style", "text-align:center;margin-top:8%;");
 contentPTag.setAttribute("style", "text-align:justify; font-size: 14pt; padding:3%; margin-top:3%;");
 contentStartBtn.setAttribute("class", "btn btn-primary");
 contentStartBtn.setAttribute("id", "start-button");
 contentStartBtn.setAttribute("style", "margin:auto; height:auto; width:20%; display:block;");
+resultPTag.setAttribute("style", "font-size: 14pt; float: left; margin:auto; height:auto; display:block; margin-bottom: 10px !important; padding: 15px;");
+
+// global array used in two functions
+optionsList = [optionA, optionB, optionC, optionD];
 
 // Display on title bar
 tbPTag.textContent = "Time: " + secondsLeft;
 
-// Countdown timer, add event listener to invoke timer
+// Countdown timer
 function setTime() {
     var timerInterval = setInterval(function () {
         tbPTag.textContent = "Time: " + secondsLeft;
@@ -87,11 +89,12 @@ function setTime() {
             endGame();
         }
     }, 1000);
-}
-// Event Listner for the button
+};
+
+// Event Listner for the start button
 var startBtnTag = document.querySelector("#start-button");
 var qIndex = -1;
-
+// start button
 startBtnTag.addEventListener("click", function () {
     // Start timer when quiz starts
     setTime();
@@ -100,97 +103,63 @@ startBtnTag.addEventListener("click", function () {
     // remove start button
     contentStartBtn.remove();
     myQuiz();
+    // append the buttons, only needs to be called once    
+    buttons();
+    // evaluate user choice, only needs to be called once
     userInput();
 });
-
+// main quiz handler
 function myQuiz() {
-    if (qIndex >= 3) {
-        endGame();
-    } else {
+    if (qIndex < (questions.length - 1)) {
         qIndex++;
-    }
+    } else { endGame(); }
     // Change title to show the question
     titleTag.textContent = questions[qIndex].q;
-    // append the buttons       
-    buttons();
-    // append choices to the 
-    optionA.textContent = questions[qIndex].options[0];
-    optionB.textContent = questions[qIndex].options[1];
-    optionC.textContent = questions[qIndex].options[2];
-    optionD.textContent = questions[qIndex].options[3];
-
-}
-
+    // append choices to the buttons
+    for (let i = 0; i < questions.length; i++) {
+        optionsList[i].textContent = questions[qIndex].options[i];
+    }
+};
+// function for when all questions are answered
 function endGame() {
     secondsLeft = 0;
     titleTag.remove();
     contentDiv.remove();
-}
+    resultPTag.remove();
 
+};
+// function to append the buttons with attributes
 function buttons() {
-    contentDiv.appendChild(optionA);
-    optionA.setAttribute("class", "btn btn-primary");
-    optionA.setAttribute("id", "ans1");
-    optionA.setAttribute("style", "margin:auto; height:auto; width:50%; display:block; margin-bottom: 10px !important; padding: 15px;");
-    contentDiv.appendChild(optionB);
-    optionB.setAttribute("class", "btn btn-primary");
-    optionB.setAttribute("id", "ans2");
-    optionB.setAttribute("style", "margin:auto; height:auto; width:50%; display:block; margin-bottom: 10px !important; padding: 15px;");
-    contentDiv.appendChild(optionC);
-    optionC.setAttribute("class", "btn btn-primary");
-    optionC.setAttribute("id", "ans3");
-    optionC.setAttribute("style", "margin:auto; height:auto; width:50%; display:block; margin-bottom: 10px !important; padding: 15px;");
-    contentDiv.appendChild(optionD);
-    optionD.setAttribute("class", "btn btn-primary");
-    optionD.setAttribute("id", "ans4");
-    optionD.setAttribute("style", "margin:auto; height:auto; width:50%; display:block; margin-bottom: 10px !important; padding: 15px;");
-}
-
+    // array created to condense code
+    idText = ["ans1", "ans2", "ans3", "ans4"];
+    for (let i = 0; i < questions.length; i++) {
+        contentDiv.appendChild(optionsList[i]);
+        optionsList[i].setAttribute("class", "btn btn-primary");
+        optionsList[i].setAttribute("id", idText[i]);
+        optionsList[i].setAttribute("style", "margin:auto; height:auto; width:50%; display:block; margin-bottom: 10px !important; padding: 15px;");
+    }
+};
+// Evaluate user inputs through the function below
+// Append if the choice is correct or wrong
 function userInput() {
-    var userChoiceA = document.getElementById("ans1");
-    var userChoiceB = document.getElementById("ans2");
-    var userChoiceC = document.getElementById("ans3");
-    var userChoiceD = document.getElementById("ans4");
+    for (let i = 0; i < questions.length; i++) {
+        var userChoice = [
+            userChoiceA = document.getElementById("ans1"),
+            userChoiceB = document.getElementById("ans2"),
+            userChoiceC = document.getElementById("ans3"),
+            userChoiceD = document.getElementById("ans4")];
 
-    userChoiceA.addEventListener("click", function () {
-        var correctChoice = document.getElementById(questions[qIndex].correctAns).id;
-        if (userChoiceA.id == correctChoice) {
-            score += 10;
-            myQuiz();
-        } else {
-            deduct();
-            myQuiz();
-        }
-    });
-    userChoiceB.addEventListener("click", function () {
-        var correctChoice = document.getElementById(questions[qIndex].correctAns).id;
-        if (userChoiceB.id === correctChoice) {
-            score += 10;
-            myQuiz();
-        } else {
-            deduct();
-            myQuiz();
-        }
-    });
-    userChoiceC.addEventListener("click", function () {
-        var correctChoice = document.getElementById(questions[qIndex].correctAns).id;
-        if (userChoiceC.id === correctChoice) {
-            score += 10;
-            myQuiz();
-        } else {
-            deduct();
-            myQuiz();
-        }
-    });
-    userChoiceD.addEventListener("click", function () {
-        var correctChoice = document.getElementById(questions[qIndex].correctAns).id;
-        if (userChoiceD.id === correctChoice) {
-            score += 10;
-            myQuiz();
-        } else {
-            deduct();
-            myQuiz();
-        }
-    });
-
+        userChoice[i].addEventListener("click", function () {
+            var correctChoice = document.getElementById(questions[qIndex].correctAns).id;
+            if (userChoice[i].id == correctChoice) {
+                resultPTag.textContent = "Correct Choice!";
+                score += 10;
+                myQuiz();
+            } else {
+                resultPTag.textContent = "Wrong choice, better luck next time";
+                deduct();
+                myQuiz();
+            }
+        });
+    }
 }
